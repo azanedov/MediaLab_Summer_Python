f9db6401d26f88c25d8f51899451b44735cc7489
Vanessa Azañedo <vanessa.azanedo@gmail.com>
2024-01-14 22:35:57 -0500

    Tasks 1 and 2
    I'm adding the tasks 1 and 2 for the assignment 4

---
+++ Notas.py
@@ -0,0 +1,30 @@
+# Funciones
+
+# len() cuenta el número de elementos
+
+# Tupla ()
+
+tuple1 = (1, 0, 3, 5, 10, 5, 5)
+
+len(tuple1)
+
+# Lista []
+
+list_1 = ["vanessa", 8, 10.5]
+
+# Para buscar []
+
+list_1[0]
+
+
+# Dict {}
+
+ Elementos dentro de llaves
+
+    claudia = {"lastname" : "Vera" , "age" : 21, "birth_place" : "Lima"}
+
+    #  Buscar información en esta lista con corchetes
+   
+    claudia["age"]
+    
+# Numpy is the core library for scientific computing in Python. It provides a high-performance multidimensional array object, and tools for working with these arrays. If you are already familiar with MATLAB, you might find this tutorial useful to get started with Numpy.
